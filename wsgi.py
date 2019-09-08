a793c3e81e4a94c65895a866db12c761e805629e
Crispy <kanday5665@gmail.com>
2019-09-08 18:32:45 +0900

    new

--- wsgi.py
+++ wsgi.py
@@ -1,10 +1,16 @@
+# This file contains the WSGI configuration required to serve up your
+# web application at http://<your-username>.pythonanywhere.com/
+# It works by setting the variable 'application' to a WSGI handler of some
+# description.
+#
+# The below has been auto-generated for your Flask project
+
 import sys
-import os
 
+# add your project directory to the sys.path
+project_home = u'/home/Crispiness/project-YOCO_WEB'
+if project_home not in sys.path:
+    sys.path = [project_home] + sys.path
 
-CURRENT_DIR = os.getcwd()
-
-sys.stdout = sys.stderr
-sys.path.insert(0, CURRENT_DIR)
-
-from app import app as application
+# import flask app but need to call it "application" for WSGI to work
+from app import app as application  # noqa
